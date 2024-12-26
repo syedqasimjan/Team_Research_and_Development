b62c6a98b6e6c4c2f1908aa6efc32d945bf68437
syedqasimjan <sj24abd@herts.ac.uk>
2024-12-26 20:41:14 +0500

    Initial setup: Loaded libraries and dataset for analysis.
    Load necessary libraries.
    Load the dataset.
    Briefly comment on the dataset's structure.

---
+++ 7COM1079-0901/Assignment2/HealthCare_Strokes.r
@@ -0,0 +1,9 @@
+# Load necessary libraries
+library(plotly) # For interactive plots
+library(ggplot2) # For data visualization
+
+# Load the dataset
+stroke_data <- read.csv("C:\\Users\\QASIM\\Downloads\\healthcare-dataset-stroke-data.csv")
+
+# Preview dataset structure
+str(stroke_data) # To understand variable types and dataset dimensions
